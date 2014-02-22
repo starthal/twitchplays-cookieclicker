30d08e1a72f6518d615343bed91a687f670a3a5c
stephen <stephen@stephen-ubu-lappy.(none)>
2014-02-22 01:49:52 -0500

    gui formatting

--- lib/gui.py
+++ lib/gui.py
@@ -21,17 +21,18 @@ def run():
     global pscroll
     
     master = Tk()
-    w4 = Text (master, background='#1E506F', foreground='white', width=40, height=10)
+    master.configure(background='#1E506F')
+    w4 = Text (master, background='#1E506F', highlightbackground='#1E506F', foreground='white', width=40, height=10, bd=0)
     w4.insert(INSERT, "-----START-----")
     w4.grid(row=0, rowspan=2, column=0)
     
     rscroll_max = config['reset_bar']['max']
-    rscroll = Scale(master, from_=rscroll_max, to=0, borderwidth=0, background='black', foreground='white', troughcolor='#FF1144')
+    rscroll = Scale(master, from_=rscroll_max, to=0, borderwidth=0, bd=0, highlightbackground='black', background='#1E506F', foreground='white', troughcolor='#FF1144')
     rscroll.grid(row=0, column=1, sticky=W+E+N+S)
     
     if config['pledge_bar']['enable']:
 		pscroll_max = config['pledge_bar']['max']
-		pscroll = Scale(master, from_=pscroll_max, to=0, borderwidth=0, background='black', foreground='white', troughcolor='white')
+		pscroll = Scale(master, from_=pscroll_max, to=0, borderwidth=0, bd=0, highlightbackground='black', background='black', foreground='white', troughcolor='white')
 		pscroll.grid(row=0, column=2, sticky=W+E+N+S)
 
     rlabel = Label(master, text="Reset", background='black', foreground='#FF1144')
