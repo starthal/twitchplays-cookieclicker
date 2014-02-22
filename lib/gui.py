ee6b9b406c316cad1f3ba5c44723dbcd1316f464
Kolo9 <darubal1@umbc.edu>
2014-02-21 21:13:16 -0500

    Updtaed GUI, Borders still exist

--- lib/gui.py
+++ lib/gui.py
@@ -12,7 +12,7 @@ def set_reset_bar(reset_counter):
 
 def command(username, command):
     global w4    
-    w4.insert(END, '\n{0:<25s} {1:>24s}'.format(username[:25], command[:14]))
+    w4.insert(END, '\n{0:<25s} {1:>14s}'.format(username[:25], command[:14]))
     w4.see(END)
 
 def run():
@@ -21,18 +21,24 @@ def run():
     global pscroll
     
     master = Tk()
-    w4 = Text (master, background='black', foreground='red', width=40, height=10)
+    w4 = Text (master, background='#1E506F', foreground='white', width=40, height=10)
     w4.insert(INSERT, "-----START-----")
-    w4.pack(fill=BOTH, expand=1, side=LEFT)
+    w4.grid(row=0, rowspan=2, column=0)
     
     rscroll_max = config['reset_bar']['max']
-    rscroll = Scale(master, from_=rscroll_max, to=0, borderwidth=0, background='black', foreground='white', troughcolor='red')
-    rscroll.pack(fill=BOTH, expand=0, side=RIGHT)
+    rscroll = Scale(master, from_=rscroll_max, to=0, borderwidth=0, background='black', foreground='white', troughcolor='#FF1144')
+    rscroll.grid(row=0, column=1, sticky=W+E+N+S)
     
     if config['pledge_bar']['enable']:
 		pscroll_max = config['pledge_bar']['max']
-		pscroll = Scale(master, from_=pscroll_max, to=0, borderwidth=0, background='black', foreground='white', troughcolor='blue')
-		pscroll.pack(fill=BOTH, expand=0, side=RIGHT)
+		pscroll = Scale(master, from_=pscroll_max, to=0, borderwidth=0, background='black', foreground='white', troughcolor='white')
+		pscroll.grid(row=0, column=2, sticky=W+E+N+S)
+
+    rlabel = Label(master, text="Reset", background='black', foreground='#FF1144')
+    rlabel.grid(row=1, column=1, sticky=W+E+N+S)
+
+    plabel = Label(master, text="Pledge\nCov", background = 'black', foreground='white')
+    plabel.grid(row=1, column=2, sticky=W+E+N+S)
  
     mainloop()
 
